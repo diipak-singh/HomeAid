3ac58f7bfdba35a9e4b642cd15d5b9d6793ee5bb
Deepak Singh Baise <deepaksinghbaise@gmail.com>
2020-04-02 14:30:10 +0530

    added support fragment successfully

--- app/src/main/java/com/tecnosols/homeaid/SupportFragment.java
+++ app/src/main/java/com/tecnosols/homeaid/SupportFragment.java
@@ -7,12 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SupportFragment extends Fragment {
+
+    private WebView webView;
+    private String url = "http://www.helpathome.co.in/contact/";
 
     public SupportFragment() {
         // Required empty public constructor
@@ -23,6 +29,13 @@ public class SupportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_support, container, false);
+        View view = inflater.inflate(R.layout.fragment_support, container, false);
+        webView = view.findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient());
+
+        return view;
     }
 }
